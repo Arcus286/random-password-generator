56a6d8e3e31377ca4873c73d1df9d5443ab7094a
Arcus286 <69086207+Arcus286@users.noreply.github.com>
2026-03-19 17:21:32 +0530

    Added the logic and finished the project Completed the js code and now the project is generating random passwords which can be used

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-const characters = [
+let characters = [
   "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
   "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
   "0","1","2","3","4","5","6","7","8","9",
   "!","@","#","$","%","^","&","*","(",")","-","_","=","+",
   "[","]","{","}","|",":",";","'","<",">",",",".","?","/"
 ];
+let firstText = document.getElementById("first-text")
+let secondText = document.getElementById("second-text")
+let generateBtn = document.getElementById("generate-btn")
+
+generateBtn.addEventListener("click", function(){
+  firstText.textContent = "";
+  secondText.textContent = "";
+  for(let i = 1;i<=15;i++){
+    firstText.textContent += characters[Math.floor(Math.random() * characters.length)];
+  }
+  for(let i = 1;i<=15;i++){
+    secondText.textContent += characters[Math.floor(Math.random() * characters.length)];
+  }
+})
